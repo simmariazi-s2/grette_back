6e666b524a2a66307279c5ddd77aeee676305cc8
jh.kim <agapeslim91.gmail.com>
2021-12-19 17:59:11 +0900

    service 추가

--- service/userService.go
+++ service/userService.go
@@ -1,7 +1,8 @@
 package service
 
 import (
-	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"work/grette_back/model"
 
@@ -41,12 +42,24 @@ func CheckUser(c *gin.Context) {
 func RegisterUser(c *gin.Context) {
 
 	user := new(model.User)
-	data, _ := json.Marshal(user)
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":    403,
+			"message": "유저정보를 확인하세요",
+		})
+
+		log.Print(err.Error())
+
+		return
+	}
+
+	fmt.Println("User :: ", user)
 
 	c.JSON(http.StatusOK, gin.H{
 		"Content-type": "application/json",
 		"message":      "회원가입",
-		"아이디":          data,
+		"아이디":          user,
 	})
 }
 
@@ -63,7 +76,15 @@ func DeleteUser(c *gin.Context) {
 }
 
 func UpdateUser(c *gin.Context) {
+
+	user := new(model.User)
+	fmt.Println("User1 :: ", user)
+	c.BindJSON(&user)
+	fmt.Println("User2 :: ", user.Email)
+	fmt.Println("User2 :: ", user.Company)
+	fmt.Println("User2 :: ", user.Password)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "비밀번호 변경 및 닉네임 변경",
+		"유저정보":    user,
 	})
 }
