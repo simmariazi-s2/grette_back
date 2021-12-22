75d2bd57eb2a1e401916582ed7c9867ae111151f
USER_20211028 <baekhk1006@gmail.com>
2021-12-22 21:29:33 +0900

    board 항목 수정

--- database/entities/boardEntity.go
+++ database/entities/boardEntity.go
@@ -1,11 +1,9 @@
 package entities
 
 type Board struct {
-	Id          int    `gorm:"primaryKey;autoIncrement;index;not null;column:id"`
-	Title       string `gorm:"column:title"`
-	Contents    string `gorm:"column:contents"`
-	MappingCode int    `gorm:"column:mappingCode"`
-	//CommentList  List<Board> `gorm:"column:commentList"`
+	Id             int    `gorm:"primaryKey;autoIncrement;index;not null;column:id"`
+	Title          string `gorm:"column:title"`
+	Contents       string `gorm:"column:contents"`
 	CategoryId     int    `gorm:"column:categoryId"`
 	CategoryName   string `gorm:"column:categoryName"`
 	UserId         string `gorm:"column:userId"`
