b9e96c5960f6a257e2069edf4e6713867e76ad5a
jh.kim <agapeslim91.gmail.com>
2022-01-09 23:13:39 +0900

    d

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"os"
 	"work/grette_back/database"
-	"work/grette_back/routers"
+	"work/grette_back/repositories"
 	"work/grette_back/setting"
+
+	"github.com/joho/godotenv"
+	_ "gorm.io/gorm"
 )
 
 func init() {
@@ -14,14 +17,46 @@ func init() {
 	database.Setup()
 }
 
+type us struct {
+	userNo       int
+	userId       string
+	userNickname string
+	companyNo    int
+	userPassword string
+}
+
 func main() {
 
-	server := &http.Server{
+	/* server := &http.Server{
 		Addr:    ":8090",
 		Handler: routers.InitRouter(),
 	}
 
 	log.Print(`[START] Grette START `, time.Now())
 
-	server.ListenAndServe()
+	server.ListenAndServe() */
+
+	envErr := godotenv.Load(".env")
+
+	if envErr != nil {
+		log.Printf(".env file Load Failed")
+	}
+	dbName := os.Getenv("DB_NAME")
+	rootID := os.Getenv("DB_ID")
+	rootPW := os.Getenv("DB_PW")
+	dbAddr := os.Getenv("DB_ADDR")
+
+	fmt.Println("dbName ::: ", dbName)
+	fmt.Println("rootID ::: ", rootID)
+	fmt.Println("rootPW ::: ", rootPW)
+	fmt.Println("dbAddr ::: ", dbAddr)
+
+	fmt.Println("db 테스트 진행중 : ", repositories.DbTest())
+	var a us
+
+	//database.Db.Raw("SELECT * FROM user").Scan(&a)
+	database.Db.Select(&a)
+
+	fmt.Println(a.companyNo)
+
 }
